913e0576bd41d6b1823bb13557571c9aee620800
Ponnesh18 <ponnesh18@gmail.com>
2026-04-28 08:45:03 +0530

    adding element and using fragment in react

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Head from './Head';
+
 
 const App = () => {
   return (
-    <div>
-      <h1>react vite project</h1>
-      <h1>we follow this method to our practice section</h1>
-    </div>
+
+    <> 
+    <Head/>
+    <h1>react vite project</h1>
+    <h1>we follow this method to our practice section</h1>
+    </>
+    //  it's called fragment in react 
+
+/* <div> 
+<Head/>
+<h1>react vite project</h1>
+<h1>we follow this method to our practice section</h1>
+</div> */    
+    
   )
 }
 
---
+++ react-vite-project/src/Head.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+       <h3>home</h3>
+       <h3>about</h3>
+       <h3>blog</h3>
+       <h3>contact</h3>
+       
+
+    </div>
+  )
+}
+
+export default Head
