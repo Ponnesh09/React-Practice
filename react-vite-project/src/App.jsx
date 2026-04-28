57f5cc00e2fa34a93c3fc72b0b47565fa89477a7
Ponnesh18 <ponnesh18@gmail.com>
2026-04-28 09:47:22 +0530

    add fragment in react

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -3,12 +3,16 @@ import Head from './Head';
 
 
 const App = () => {
-  return (
 
+  const userName = "petter"
+
+  return (
     <> 
     <Head/>
     <h1>react vite project</h1>
     <h1>we follow this method to our practice section</h1>
+    {/* we inject variable inside App.jsx   using JS   */}
+    <h1> hii i am {userName}</h1>
     </>
     //  it's called fragment in react 
 
