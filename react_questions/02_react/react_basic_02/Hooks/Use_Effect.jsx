140788c7684d665cddb7ec3dcdb6e6cba3c88cc3
Ponnesh18 <ponnesh18@gmail.com>
2026-07-16 10:17:02 +0530

    use Effect demo

---
+++ react_questions/02_react/react_basic_02/Hooks/Use_Effect.jsx
@@ -0,0 +1,22 @@
+// 2 useEffect useEffectHandles side effects like data fetching, subscriptions, or manual DOM updates.
+
+import React, { useEffect, useState } from 'react'
+
+const Use_Effect = () => {
+
+const [data, setData] = useState(null)
+
+useEffect(()=>{
+    fetch("https://github.com")
+    .then(res=>res.json)
+    .then(data => setData(data))
+},[])// Empty array means this runs once on mount
+
+return (
+    
+      <div> {data ? data.name : 'Loading....'}</div>
+
+  )
+}
+
+export default Use_Effect
--- react_questions/02_react/react_basic_02/src/App.jsx
+++ react_questions/02_react/react_basic_02/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Use_State from '../Hooks/Use_State'
+import Use_Effect from '../Hooks/Use_Effect'
 
 const App = () => {
   return (
     <div>
         <Use_State />
+        <Use_Effect />
     </div>
   )
 }
