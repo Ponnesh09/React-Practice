23541a5389899ca613c748727ff409cfc837218e
Ponnesh18 <ponnesh18@gmail.com>
2026-07-17 12:24:43 +0530

    2nd example done for Lifting State Up

---
+++ react_questions/03_react/components/CounterButtons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CounterButtons = ({ count, setCount }) => {
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </>
+  );
+};
+
+export default CounterButtons;
